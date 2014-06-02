3d221858effeebea1132a0b368da14567c97e9c5
Alexander Knapstein <alexander.knapstein@inspirationlabs.com>
2014-06-02 18:25:07 +0200

    execution helper, adding preprocess function on query
    Signed-off-by: Alexander Knapstein <alexander.knapstein@inspirationlabs.com>

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * INSPIRATIONlabs GmbH
  * http://www.inspirationlabs.com
  */
-//var _ = require('underscore');
+var _ = require('underscore');
 var fs = require('fs');
 var collquery = require ('./lib/query');
 //var async = require('async');
@@ -108,6 +108,40 @@ collections.prototype.query = function query(name, options) {
   var currentQuery = new collquery(this.elastic, new this.getCollection(name), options);
   return currentQuery;
 };
+
+collections.prototype.exec = function exec(name, options, callback) {
+  var self = this;
+  var query = self.query(name, options);
+  query.exec(function (err, result) {
+    if(err) {
+      return callback(err);
+    }
+    return self.processResult(result, callback);
+  });
+};
+
+collections.prototype.processResult = function(query, result, callback) {
+  var r = { total: 0, limit: query.query.limit, from: query.query.from, items: [] };
+  r._meta = {
+    module: 'collections',
+    type: 'collection'
+  };
+
+  if (result && result.hits && result.hits.hits && result.hits.hits[0]) {
+    r.total = result.hits.total;
+    _.each(result.hits.hits, function(hit) {
+      var source = hit._source;
+      source._meta = {
+        module: 'elements',
+        type: hit._type
+      };
+      hit._view = { template: hit._type };
+      r.items.push(source);
+    });
+  }
+  return callback(null, r);
+};
+
 
 var init = function (m) {
   // store molecuel instance
